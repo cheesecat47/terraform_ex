09612364e25bf105e07750fed00812c0242a426c
cheesecat47 <cheesecat47@gmail.com>
2024-08-26 13:43:42 +0900

    run script silent - remove percentage bar while installing

--- 01.Terraform/ansible/install_ansible.sh
+++ 01.Terraform/ansible/install_ansible.sh
@@ -2,10 +2,10 @@
 
 # Install pipx
 # https://pipx.pypa.io/stable/#on-linux
-sudo apt -qq update
-sudo apt -qq -y install pipx
-pipx ensurepath
+sudo apt -qq update > /dev/null
+sudo apt -qq -y install pipx > /dev/null
+pipx ensurepath > /dev/null
 
 # Install ansible using pipx
 # https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html#pipx-install
-pipx install -qq --include-deps ansible
+pipx install -qq --include-deps ansible > /dev/null
